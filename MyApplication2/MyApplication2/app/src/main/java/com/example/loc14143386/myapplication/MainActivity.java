10a3aaffc6cbef33d9b7944f498508d9e060b8f1
Samtastic95 <LOC14143386@COBC33314.CityBathColl.ac.uk>
2015-10-07 11:27:54 +0100

    Added answers to true and false buttons.
    Added answers to 'true' and 'false buttons.

--- MyApplication2/MyApplication2/app/src/main/java/com/example/loc14143386/myapplication/MainActivity.java
+++ MyApplication2/MyApplication2/app/src/main/java/com/example/loc14143386/myapplication/MainActivity.java
@@ -1,37 +1,66 @@
 package com.example.loc14143386.myapplication;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //variables go here
+    private Button btnFalse;
+    private Button btnTrue;
+    private TextView tblQuestion;
+    private ImageView imgPicture;
+
+   // private List<QuestionObject> questions;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        btnFalse = (Button)findViewById(R.id.btnFalse);
+        btnTrue = (Button) findViewById(R. id.btnTrue);
+        tblQuestion = (TextView) findViewById(R. id. lblQuestion);
+        imgPicture = (ImageView) findViewById(R. id. imgPicture);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
-}
+        //Oncclick Listeners
+
+        btnFalse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //wrong!!!
+                Toast.makeText(MainActivity.this, "wrong!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnTrue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //correct!
+                Toast.makeText(MainActivity.this, "correct!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    } // ends onCreate
+
+
+} //ends activity
+
+
+
+
+
+
+
+
---
+++ MyApplication2/MyApplication2/app/src/main/java/com/example/loc14143386/myapplication/QuestionObject.java
@@ -0,0 +1,35 @@
+package com.example.loc14143386.myapplication;
+
+/**
+ * Created by LOC14143386 on 07/10/2015.
+ */
+
+public class QuestionObject {
+
+    private String question;
+    private boolean answer;
+    private int picture;
+
+    Public QuestionObject(String question, boolean answer, int picture) {
+
+        this.question = question;
+        this.answer = answer;
+        this.picture = picture;
+    }
+    public String getQuestion() {
+        return Question;
+    }
+    public boolean isAnswer() {
+return answer
+    }
+    public int getPicture() {
+
+        return picure;
+
+    }
+
+
+    }
+
+
+
